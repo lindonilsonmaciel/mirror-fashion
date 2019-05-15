cf1f9f25b83801666dab036513da530912a65d1c
Lindonilson Maciel <lindonilsonmaciel@gmail.com>
2019-05-15 17:33:47 -0300

    Resolvido bug no range que não mudava

--- js/produto.js
+++ js/produto.js
@@ -1,8 +1,5 @@
-var inputTamanho = document.querySelector('[name=tamanho]');
-var outputTamanho = document.querySelector('[name="valortamanho"]');
-console.log(inputTamanho, outputTamanho)
 function mostraTamanho(){
-    outputTamanho.value = inputTamanho.value
+    var inputTamanho = document.querySelector('#tamanho');
+    var outputTamanho = document.querySelector('#valorTamanho');
+    outputTamanho.value = inputTamanho.value;
 }
-
-inputTamanho.oninput = mostraTamanho
